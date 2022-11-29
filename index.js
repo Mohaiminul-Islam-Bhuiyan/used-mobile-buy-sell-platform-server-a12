0362cb263ae1772cd98554b4c0e9857cb9e050c3
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-11-29 19:10:19 +0600

    produt according to category sent

--- index.js
+++ index.js
@@ -16,6 +16,39 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const categoryCollection = client.db('mobileHaat').collection('categories')
+        const productsCollection = client.db('mobileHaat').collection('products')
+
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const cursor = categoryCollection.find(query)
+            const categories = await cursor.toArray()
+            res.send(categories)
+        })
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id
+            // console.log(id);
+            const query = {}
+            // const query = { category_id: id }
+            const cursor = productsCollection.find(query)
+            const products = await cursor.toArray()
+
+            const matchedProducts = products.filter(n => n.category_id == id);
+            res.send(matchedProducts);
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run()
+    .catch(err => console.log(err))
+
 
 
 
