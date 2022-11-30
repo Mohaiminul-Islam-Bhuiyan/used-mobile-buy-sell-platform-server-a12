91e4c1213d25694c18688d12693438d323eae6fd
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-11-30 18:03:54 +0600

    manage products api added

--- index.js
+++ index.js
@@ -109,6 +109,34 @@ async function run() {
         })
 
 
+        //for  insert products
+
+        app.get('/productCategory', async (req, res) => {
+            const query = {}
+            const result = await categoryCollection.find(query).project({ category_id: 1 }).toArray()
+            res.send(result)
+        })
+
+        app.post('/products', async (req, res) => {
+            const product = req.body
+            const result = await productsCollection.insertOne(product)
+            res.send(result)
+        })
+
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const products = await productsCollection.find(query).toArray()
+            res.send(products)
+        })
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await productsCollection.deleteOne(filter)
+            res.send(result)
+        })
+
+
 
     }
     finally {
